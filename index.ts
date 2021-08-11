8c44eeca14be91ac37c78e293ee49afa8b005d11
Vinicius Dias <carlosv775@gmail.com>
2021-08-11 01:08:20 -0300

    Habilitando cors

--- index.ts
+++ index.ts
@@ -2,8 +2,11 @@ import leads from './leads/ui/express-leads-router'
 import config from './env-config';
 
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/leads', leads);
 
